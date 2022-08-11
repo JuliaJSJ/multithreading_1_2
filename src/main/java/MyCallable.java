0c74bb9d722bba4bca3f182c4a242b2fc8987eec
JuliaJSJ <80544651+JuliaJSJ@users.noreply.github.com>
2022-08-11 10:50:15 -0400

    Create MyCallable.java

---
+++ src/main/java/MyCallable.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.Callable;
+
+public class MyCallable implements Callable<Integer> {
+    private final String name;
+
+    public MyCallable(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        int count = 0;
+        Thread.currentThread().setName(name);
+
+        while (count <= 3) {
+            Thread.sleep(2500);
+            System.out.println("Всем привет! " + "Я поток " + Thread.currentThread().getName());
+            count++;
+        }
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
